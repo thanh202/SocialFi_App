170cbd891755611b065f2ce8d035ce53419e542f
vanh6803 <anhnv6083@gmail.com>
2024-03-22 23:08:42 +0700

    update file mainActivity.kt

--- android/app/src/main/java/com/socialfi_app/MainActivity.kt
+++ android/app/src/main/java/com/socialfi_app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.socialfi_app
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,6 +11,10 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+ override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+
   override fun getMainComponentName(): String = "SocialFi_App"
 
   /**
